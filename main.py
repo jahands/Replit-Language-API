f47264c00bd602b0ee478b10e0abca57db47e77f
Jacob Hands <jacob@gogit.io>
2021-05-19 18:55:07 +0000

    Add error handling

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 
 from lib import get_languages_cached
+from dblog import dblog
+from logzero import logger
 
 app = FastAPI()
 # FastAPI docs: https://fastapi.tiangolo.com/
@@ -11,12 +13,16 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return RedirectResponse("https://Replit-Language-API.jahands.repl.co/docs", 307)
+    return RedirectResponse("https://Replit-Language-API.jahands.repl.co/docs",
+                            307)
 
 
 @app.get("/api/languages")
 async def get_languages():
-    return {
-        "api_version": 1,
-        "languages": json.loads(get_languages_cached())
-    }
+    try:
+        languages = get_languages_cached()
+    except Exception as e:
+        logger.exception(e)
+        dblog(e)
+        raise HTTPException(status_code=500, detail="internal server error")
+    return {"api_version": 1, "languages": json.loads(languages)}
