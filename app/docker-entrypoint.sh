60a72b18e0e8e7045b398cb7d7924b3774342287
Jacob Hands <jacob@gogit.io>
2021-11-26 03:02:31 +0000

    2 workers

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec uvicorn main:app --port $PORT --host 0.0.0.0 --workers 4 -k uvicorn.workers.UvicornWorker
+exec uvicorn main:app --port $PORT --host 0.0.0.0 --workers 2 -k uvicorn.workers.UvicornWorker
